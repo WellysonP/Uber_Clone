3587926bfb4f70c77a824cefb32dda05f3bd6b32
Wellyson Pontes <wellysonpontes@gmail.com>
2022-06-30 16:08:49 -0300

    codigo inicial testando firebase database com função get

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:car_rider/screens/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MainPage(),
-    );
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const MainPage(),
+  ));
 }
--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,6 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -13,8 +12,18 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Main Page"),
+      appBar: AppBar(title: Text("Main Page")),
+      body: Center(
+        child: MaterialButton(
+          height: 50,
+          minWidth: 300,
+          color: Colors.green,
+          child: Text("Test Connection"),
+          onPressed: () {
+            DatabaseReference dbRef = FirebaseDatabase.instance.ref("test");
+            dbRef.set("Test Connection");
+          },
+        ),
       ),
     );
   }
